9d426b43bbe5170626f8e2f32618b1b981e193ed
NikitaGlukhi <nikita.glukhi@valor-software.com>
2020-12-03 18:35:58 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'firebase-preview-channels-commenting-with-travis-ci';
+}
